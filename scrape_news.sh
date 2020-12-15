6d33740fdd36b57652915ec267f6216cb6ddb94a
antoan12 <75024404+antoan12@users.noreply.github.com>
2020-12-15 16:08:26 +0200

    Update scrape_news.sh

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# seperating individual links from html "3082",searching for specified ones 
 wget https://www.ynetnews.com/category/3082
 less 3082 | grep -o "https://www.ynetnews.com/article/[[:alnum:]]*" | sort -u > result.txt
 num=$(less result.txt | wc -l)
 echo $num
 arr=($(less result.txt))
+# run on links. get num of times Netanyahu/Gantz mentioned.
 for (( i=0; i<$num;i++ ));
 do
 	wget -O temp.txt ${arr[i]} &> /dev/null
