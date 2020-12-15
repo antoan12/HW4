0e7d079c4d207eb11203dd0bde4f549d9732d034
antoan12 <75024404+antoan12@users.noreply.github.com>
2020-12-15 15:51:27 +0200

    Add files via upload

---
+++ scrape_news.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+wget https://www.ynetnews.com/category/3082
+less 3082 | grep -o "https://www.ynetnews.com/article/[[:alnum:]]*" | sort -u > result.txt
+num=$(less result.txt | wc -l)
+echo $num
+arr=($(less result.txt))
+for (( i=0; i<$num;i++ ));
+do
+	wget -O temp.txt ${arr[i]} &> /dev/null
+	count_netanyahu=$(less temp.txt | grep  -o Netanyahu | wc -l)
+	count_gantz=$(less temp.txt | grep  -o Gantz | wc -l )
+	if [[ $count_netanyahu == 0  &&  $count_gantz == 0 ]]; 
+	then
+		echo "${arr[i]},-"
+
+	else echo "${arr[i]},Netanyahu,$count_netanyahu,Gantz,$count_gantz"
+	fi
+done
